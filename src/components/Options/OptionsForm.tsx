edaff7a2e9200178d3a3fa7e3cc7b819b1641e7b
fungikami <kafung09@gmail.com>
2023-05-29 16:55:10 -0400

    Fix options html

--- src/components/Options/OptionsForm.tsx
+++ src/components/Options/OptionsForm.tsx
@@ -2,121 +2,150 @@
  * Credibility parameters component
  *
  */
-// import React from "react";
+import React from "react";
+// import constants from './constants'
+
+const labels = [
+  {
+    id: 1,
+    title: "Customize text credibility parameters",
+    inputs: [
+      {
+        id: 1,
+        title: "Spam detection",
+        placeholder: "Spam",
+        type: "text",
+        name: "weightSpam",
+      },
+      {
+        id: 2,
+        title: "Bad words proportion to text",
+        placeholder: "Bad words",
+        type: "text",
+        name: "weightBadWords",
+      },
+      {
+        id: 3,
+        title: "Misspelling detection",
+        placeholder: "Misspelling",
+        type: "text",
+        name: "weightMisspelling",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Customize tweet credibility parameters",
+    inputs: [
+      {
+        id: 1,
+        title: "Text credibility",
+        placeholder: "Text",
+        type: "text",
+        name: "weightText",
+      },
+      {
+        id: 2,
+        title: "User credibility",
+        placeholder: "User",
+        type: "text",
+        name: "weightUser",
+      },
+      {
+        id: 3,
+        title: "Social credibility",
+        placeholder: "Social",
+        type: "text",
+        name: "weightSocial",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Max followers parameter",
+    inputs: [
+      {
+        id: 1,
+        title: "Max followers",
+        placeholder: "Max followers",
+        type: "text",
+        name: "maxFollowers",
+      },
+    ],
+  },
+];
 
 function OptionsForm() {
+  const [inputs, setInputs] = React.useState({
+    weightSpam: "",
+    weightBadWords: "",
+    weightMisspelling: "",
+    weightText: "",
+    weightUser: "",
+    weightSocial: "",
+    maxFollowers: "",
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(inputs);
+
+    // Guarda en Chrome Storage
+    chrome.storage.sync.set({ inputs: inputs }, function () {
+      console.log("Inputs saved");
+    });
+
+  };
+
   return (
     <div className="container p-2.5 ">
-      <h1 className="title text-2xl font-bold">
-        Customize text credibility parameters
+      <h1 className="title text-2xl font-bold mb-4">
+        T-Creo v2.0 - Credibility parameters
       </h1>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="col-sm-6 p-2 p-2">
-          <h3 className="title text-xl font-bold">Filters</h3>
-          <div>
-            <p>Spam detection</p>
+      <form onSubmit={handleSubmit}>
+        {labels.map((label) => (
+          <div key={label.id}>
+            <h2 className="title text-xl font-bold">{label.title}</h2>
+            {label.inputs.map((input) => (
+              <div className="md:flex md:items-center my-3" key={input.id}>
+                <div className="md:w-1/3">
+                  <label
+                    htmlFor=""
+                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  >
+                    {input.title}
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    type={input.type}
+                    id={input.name}
+                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                    placeholder={input.placeholder}
+                    onChange={(event) =>
+                      setInputs({
+                        ...inputs,
+                        [input.name]: event.target.value,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+            ))}
           </div>
-          <div>
-            <p>Bad words proportion to text</p>
-          </div>
-          <div>
-            <p>Misspelling detection</p>
-          </div>
-        </div>
-        <div className="col-sm-6 p-2">
-          <h3>Weights</h3>
-          <div>
-            <input
-              type="text"
-              id="weightSpam"
-              className="form-control"
-              placeholder="Spam"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="weightBadWords"
-              className="form-control"
-              placeholder="Bad words"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="weightMisspelling"
-              className="form-control"
-              placeholder="Misspelling"
-            />
-          </div>
-        </div>
-        <div className="col-sm"></div>
-      </div>
-      <h1 className="title text-2xl font-bold">
-        Customize tweet credibility parameters
-      </h1>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="col-sm-6 p-2">
-          <h3 className="title text-xl font-bold">Filters</h3>
-          <div>
-            <p>Text credibility</p>
-          </div>
-          <div>
-            <p>User credibility</p>
-          </div>
-          <div>
-            <p>Social credibility</p>
+        ))}
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button
+              className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Save Weights
+            </button>
           </div>
         </div>
-        <div className="col-sm-6 p-2">
-          <h3>Weights</h3>
-          <div>
-            <input
-              type="text"
-              id="weightText"
-              className="form-control"
-              placeholder="Text"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="weightUser"
-              className="form-control"
-              placeholder="User"
-            />
-          </div>
-          <div>
-            <input
-              type="text"
-              id="weightSocial"
-              className="form-control"
-              placeholder="Social"
-            />
-          </div>
-        </div>
-        <div className="col-sm"></div>
-      </div>
-      <h1 className="title text-2xl font-bold">Max followers parameter</h1>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="col-sm-6 p-2">
-          <div>
-            <p>Max followers</p>
-          </div>
-        </div>
-        <div className="col-sm-6 p-2">
-          <div>
-            <input
-              type="text"
-              id="maxFollowers"
-              className="form-control"
-              placeholder="Max followers"
-            />
-          </div>
-        </div>
-        <div className="col-sm"></div>
-      </div>
-      <input type="submit" id="SaveWeights" value="Save Weights" />
+      </form>
     </div>
   );
 }
---
+++ src/components/Options/constants.ts
@@ -0,0 +1,17 @@
+const WEIGHT_SPAM = 'weightSpam'
+const WEIGHT_BAD_WORDS = 'weightBadWords'
+const WEIGHT_MISSPELLING = 'weightMisspelling'
+const WEIGHT_TEXT = 'weightText'
+const WEIGHT_USER = 'weightUser'
+const WEIGHT_SOCIAL = 'weightSocial'
+const MAX_FOLLOWERS = 'maxFollowers'
+
+export default {
+  WEIGHT_SPAM,
+  WEIGHT_BAD_WORDS,
+  WEIGHT_MISSPELLING,
+  WEIGHT_TEXT,
+  WEIGHT_USER,
+  WEIGHT_SOCIAL,
+  MAX_FOLLOWERS,
+}
