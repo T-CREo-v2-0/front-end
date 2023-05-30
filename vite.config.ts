b8763cc5ac09bbbf70110515104c4758b3ae5124
fungikami <kafung09@gmail.com>
2023-05-30 14:35:44 -0400

    Fix Options extension

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,28 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
+// const root = resolve(__dirname, 'src')
+// const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
+  // root,
   plugins: [react()],
+  // build: {
+  //   outDir,
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(root, 'index.html'),
+  //       options: resolve(root, 'options', 'options.html'),
+  //     },
+  //   },
+  // },
   build: {
-    outDir,
-    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(root, 'index.html'),
-        options: resolve(root, 'options', 'index.html'),
+        main: resolve(__dirname, 'index.html'),
+        options: resolve(__dirname, 'options.html'),
       },
     },
   },
